ee0a7d8ff16b2ac262465c2ff41c25f0b43c9955
Kul <kultar@kultar.net>
2016-08-02 16:02:08 -0400

    adding readme and alert-status script

---
+++ README.md
@@ -0,0 +1,8 @@
+# Datadog-Scripts
+
+This repo contains any scripts that i've whipped together to help with datadog integrations
+
+
+### alert-status.sh
+This script queries your monitors and prints a simple status output.
+As a note - if you have a multi-check alert configured, this will only print the overall status of the check and not the individual status.
---
+++ alert-status.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Make sure you replace the API and/or APP key below
+# with the ones for your account
+
+api_key=
+app_key=
+ret=""
+
+
+$(curl -G "https://app.datadoghq.com/api/v1/monitor" \
+     -d "api_key=${api_key}" \
+     -d "application_key=${app_key}" >/tmp/dd_status )
+
+
+count=`jq '.|length' < /tmp/dd_status `
+count=`expr $count - 1`
+
+for f in `seq 0 ${count}`
+do
+	
+	name=$(jq .[$f].name /tmp/dd_status)
+	id=$(jq .[$f].id /tmp/dd_status)
+	state=$(jq .[$f].overall_state /tmp/dd_status)
+
+	if [[ $state == '"Warn"' ]];then
+	  ret="${ret}${name} : $state \n"
+	elif [[ $state == '"Alert"' ]]; then
+	  ret="${ret}${name} : $state \n"
+	fi
+	
+done
+
+if [[ -z $ret ]]; then
+	echo "All systems normal"
+else
+    echo -e $ret
+fi
+
+
